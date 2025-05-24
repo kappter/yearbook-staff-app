f3dc2275730ebef5651587cd404f915869723315
Ken Kapptie <kkapptie@graniteschools.org>
2025-05-24 17:35:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 
+gapi.client.load('sheets', 'v4', () => {
+  console.log('Sheets API loaded');
+});
+
+async function appendTask(taskData) {
+  const response = await gapi.client.sheets.spreadsheets.values.append({
+    spreadsheetId: '<YOUR_SHEET_ID>',
+    range: 'Sheet1!A1',
+    valueInputOption: 'RAW',
+    insertDataOption: 'INSERT_ROWS',
+    values: [[
+      taskData.userEmail,
+      taskData.userName,
+      taskData.team,
+      taskData.taskType,
+      taskData.description,
+      taskData.artifactLink || '',
+      taskData.timeSpent || '',
+      taskData.status,
+      taskData.editorNotes || '',
+      new Date().toISOString(),
+      taskData.editorEmail || ''
+    ]]
+  });
+  return response;
+}

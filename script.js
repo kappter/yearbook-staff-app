521d2e1b06eed065061cb3985dddc00b1ccdeafe
Ken Kapptie <kkapptie@graniteschools.org>
2025-05-24 17:52:24 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
-
-gapi.client.load('sheets', 'v4', () => {
-  console.log('Sheets API loaded');
-});
-
-async function appendTask(taskData) {
-  const response = await gapi.client.sheets.spreadsheets.values.append({
-    spreadsheetId: '<YOUR_SHEET_ID>',
-    range: 'Sheet1!A1',
-    valueInputOption: 'RAW',
-    insertDataOption: 'INSERT_ROWS',
-    values: [[
-      taskData.userEmail,
-      taskData.userName,
-      taskData.team,
-      taskData.taskType,
-      taskData.description,
-      taskData.artifactLink || '',
-      taskData.timeSpent || '',
-      taskData.status,
-      taskData.editorNotes || '',
-      new Date().toISOString(),
-      taskData.editorEmail || ''
-    ]]
+function initGoogleAuth() {
+  gapi.load('client:auth2', () => {
+    gapi.client.init({
+      clientId: '782915328509-4joueiu50j6kkned1ksk1ccacusblka5.apps.googleusercontent.com',
+      scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/spreadsheets',
+      redirect_uri: 'https://kappter.github.io/yearbook-staff-app/',
+      hosted_domain: 'graniteschools.org'
+    }).then(() => {
+      const auth = gapi.auth2.getAuthInstance();
+      auth.isSignedIn.listen(updateSignInStatus);
+      updateSignInStatus(auth.isSignedIn.get());
+      document.getElementById('login-btn').onclick = () => auth.signIn();
+    }, (error) => {
+      console.error('Auth initialization error:', error);
+    });
   });
-  return response;
 }
+
+function updateSignInStatus(isSignedIn) {
+  if (isSignedIn) {
+    const profile = gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile();
+    document.getElementById('user-info').innerText = `Welcome, ${profile.getName()} (${profile.getEmail()})`;
+    localStorage.setItem('userEmail', profile.getEmail());
+    localStorage.setItem('userName', profile.getName());
+  } else {
+    document.getElementById('user-info').innerText = '';
+  }
+}
+
+window.onload = initGoogleAuth;

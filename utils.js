9f03b8f94ea1dda3b23227468c38e970cf03c91f
Ken Kapptie <kkapptie@graniteschools.org>
2025-05-25 07:51:37 -0600

    Update utils.js

--- utils.js
+++ utils.js
@@ -20,7 +20,21 @@ async function appendTask(accessToken, taskData) {
   const response = await fetch('https://sheets.googleapis.com/v4/spreadsheets/1Eca5Bjc1weVose02_saqVUnWvoYirNp1ymj26_UY780/values/Sheet1!A1:append?valueInputOption=RAW&insertDataOption=INSERT_ROWS', {
     method: 'POST',
     headers: { Authorization: `Bearer ${accessToken}`, 'Content-Type': 'application/json' },
-    body: JSON.stringify({ values: [[...Object.values(taskData)]] })
+    body: JSON.stringify({
+      values: [[
+        taskData.userEmail,
+        taskData.userName,
+        taskData.team,
+        taskData.taskType,
+        taskData.description,
+        taskData.artifactLink || '',
+        taskData.timeSpent || '',
+        taskData.status,
+        taskData.editorNotes || '',
+        new Date().toISOString(),
+        taskData.editorEmail || ''
+      ]]
+    })
   });
   return response.json();
 }
